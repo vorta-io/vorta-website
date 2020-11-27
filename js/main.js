4093194a5b157dba3edf29b65afdb1136302bc07
Haris Krajina <haris.krajina@gmail.com>
2020-11-27 15:57:26 +0100

    New changes to website

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  function init() {
+    $window = $(window);
+    $body = $('body');
+    $nav = $('.navbar');
+    $navOffsetTop = $nav.offset().top;
+
+    $window.on('scroll', onScroll);
+    $window.on('resize', resize);
+  }
+
+  function resize() {
+    $body.removeClass('has-docked-nav')
+    $navOffsetTop = $nav.offset().top
+    onScroll()
+  }
+  
+  function onScroll() {
+    if($navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
+      $body.addClass('has-docked-nav')
+    }
+    if($navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
+      $body.removeClass('has-docked-nav')
+    }
+  }
+
+  init();
+});
+
