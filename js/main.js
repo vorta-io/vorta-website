3c3e8f039a54bdbd325629bc5f65c56a85de6ed4
Haris Krajina <haris.krajina@gmail.com>
2021-01-28 13:13:34 +0100

    Try new version

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
       locale: 'en', //This is the default value
       timeout: 500, //Time until the cookie bar appears
       expiration: 30, //This is the default value, in days
-      domain: '.netlify.app', //If you run the same cookie notice on all subdomains, define the main domain starting with a .
+      domain: 'https://quirky-hypatia-4de574.netlify.app/', //If you run the same cookie notice on all subdomains, define the main domain starting with a .
       implicit: true, //Accept cookies on page scroll automatically
-      statement: 'https://google.com', //Link to your cookie statement page
+      statement: '', //Link to your cookie statement page
       performance: ['JSESSIONID'], //Cookies in the performance category.
       analytics: ['ga'], //Cookies in the analytics category.
       marketing: ['SSID'] //Cookies in the marketing category.
