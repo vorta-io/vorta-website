20eba5034ac4ddb5bf306288d53e9847e138941b
Haris Krajina <haris.krajina@gmail.com>
2021-01-28 13:51:07 +0100

    Fix issue with cookies

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
   function cookie() {
-    gdprCookieNotice({
-      locale: 'en', //This is the default value
-      timeout: 500, //Time until the cookie bar appears
-      expiration: 30, //This is the default value, in days
-      domain: 'https://quirky-hypatia-4de574.netlify.app/', //If you run the same cookie notice on all subdomains, define the main domain starting with a .
-      implicit: true, //Accept cookies on page scroll automatically
-      statement: '', //Link to your cookie statement page
-      performance: ['JSESSIONID'], //Cookies in the performance category.
-      analytics: ['ga'], //Cookies in the analytics category.
-      marketing: ['SSID'] //Cookies in the marketing category.
-    });
+    if (!Cookies.get('vortaGdprAccepted')) {
+      gdprCookieNotice({
+        locale: 'en', //This is the default value
+        timeout: 500, //Time until the cookie bar appears
+        expiration: 30, //This is the default value, in days
+        domain: 'https://quirky-hypatia-4de574.netlify.app/', //If you run the same cookie notice on all subdomains, define the main domain starting with a .
+        implicit: true, //Accept cookies on page scroll automatically
+        statement: '', //Link to your cookie statement page
+        performance: ['JSESSIONID'], //Cookies in the performance category.
+        analytics: ['ga'], //Cookies in the analytics category.
+        marketing: ['SSID'] //Cookies in the marketing category.
+      });
+    }
   }
 
   cookie();
 });
 
+
+document.addEventListener("click", function(element) {
+  if (element.target.classList.contains('js-cookie-accepted')) {
+    Cookies.set('vortaGdprAccepted', true, { expires: 30 })
+  }
+});
